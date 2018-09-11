4a710d3e4ba8417ad0b7fdffae74f8701481431c
Jay.Lee <kkomarocker@gmail.com>
2018-09-11 13:03:48 -0400

    feat(dependencies): dependencies and components added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { Router } from "react-router-dom";
+import store from "./store";
 import "./index.css";
 import Home from "./Components/Home";
 import registerServiceWorker from "./registerServiceWorker";
